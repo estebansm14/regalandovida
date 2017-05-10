7aa92c98b15f3b522ddde4c0c3aee656b7eb85a2
jalcara1 <jalcara1@eafit.edu.co>
2017-05-10 11:02:51 -0500

    Peticiones POST y modificando consultas

--- regalandoVida/app.js
+++ regalandoVida/app.js
@@ -2,66 +2,72 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+app.use(logger('dev'));
 var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 var mongoose = require("mongoose");
 //mongoose.Promise = global.Promise;
+/* var cookieParser = require('cookie-parser');
+ * app.use(cookieParser());*/
 var bancoSangre = require('./model/bancoSangre').bancoSangre;
 //var hospital = require('./model/hospital').hospital;
 //var usuario = require('./model/usuario').usuario;
-
-app.get('/', function(req, res){
-	res.sendfile('./public/index.html');
-    });
-app.get("/api/menuBanco", function(req,res){
-	bancoSangre.find({idbanco:"bClinicaAmericas"},"encargado telefono localizacion.direccion",function(err,docs){
-		console.log("Consulta Informacion Banco Sangre\n");
-		console.log(docs);
-		res.json(docs);
-		//docs.telefono = "1111111";
-	    });
-    });
-app.get("/api/estadoReserva", function(req,res){
-	bancoSangre.find({idbanco:"bClinicaAmericas"},"tipo_de_sangre",function(err,docs){
-		console.log("Consulta Estado Reservas Sangre\n");
-		console.log(docs);
-		res.json(docs);
-	    });
-    });
-app.post("/api/estadoReserva/:id", function(req,res){
-	//var yourModel = req.body;
-	//console.log("Consulta: " + yourModel);
-	res.json(yourModel)
-
-	    });
-app.get("/api/buscarBanco", function(req,res){
-	bancoSangre.find({idbanco:"bClinicaAmericas"},"tipo_de_sangre",function(err,docs){
-		console.log("Consulta Estado Reserva Sangre Banco\n");
-		console.log(docs);
-		res.json(docs);
-	    });
-    });
-app.get("/api/solicitudesBanco", function(req,res){
-	bancoSangre.find({idbanco:"bClinicaAmericas"},"solicitudes_banco",function(err,docs){
-		console.log("Consulta Estado Reserva Sangre Banco\n");
-		console.log(docs);
-		res.json(docs);
-	    });
-    });
 app.post("/Peticion", function(req,res){
-	var yourModel = req.body;
-	console.log("Consulta: " + yourModel);
-	res.json(yourModel)
-
-	    });
-app.post("/Imprimir", function(req,res){
-	bancoSangre.find({idbanco:req.body.encargado},function(err,docs){
-		//console.log(req.query.encargado);
-		console.log("Consulta: " + req.body.encargado);
-		res.send(docs);
-	    });
+    //var Peticion = req.body;
+    console.log("Aqui Envio Una Peticion A El Servidor");
+    console.log(req.body);
+    //var yourModel = req.body.encargado;
+    //console.log("Consulta: " + yourModel);
+    res.send("recieved your request!");
+});
+app.get('/', function(req, res){
+    res.sendfile('./public/index.html');
+});
+app.get("/api/menuBanco", function(req,res){    
+    //bancoSangre.find({idbanco:"bClinicaAmericas"},"encargado telefono localizacion.direccion",function(err,docs){
+    console.log("Consulta Informacion Banco Sangre\n");
+    bancoSangre.find({idbanco:req.body.idbanco},"encargado telefono localizacion.direccion",function(err,docs){
+	console.log(docs);
+	res.json(docs);
+	//docs.telefono = "1111111";
     });
+});
+app.get("/api/estadoReserva", function(req,res){
+    console.log("Consulta Estado Reservas Sangre\n");
+    bancoSangre.find({idbanco:req.body.idbanco},"tipo_de_sangre",function(err,docs){
+	console.log(docs);
+	res.json(docs);
+    });
+});
+app.post("/api/estadoReserva", function(req,res){    
+    //var yourModel = req.body;
+    //console.log("Consulta: " + yourModel);
+    res.json(yourModel);
+});
+app.get("/api/buscarBanco", function(req,res){
+    	console.log("Consulta Estado Reserva Sangre Banco\n");
+    bancoSangre.find({idbanco:req.body.idbanco},"tipo_de_sangre",function(err,docs){
+	console.log(docs);
+	res.json(docs);
+    });
+});
+app.get("/api/solicitudesBanco", function(req,res){
+    	console.log("Consulta Solicitudes Banco\n");
+    bancoSangre.find({idbanco:req.body.idbanco},"solicitudes_banco",function(err,docs){
+	console.log(docs);
+	res.json(docs);
+    });
+});
+app.post("/Imprimir", function(req,res){
+    bancoSangre.find({idbanco:req.body.encargado},function(err,docs){
+	//console.log(req.query.encargado);
+	console.log("Consulta: " + req.body.encargado);
+	res.send(docs);
+    });
+});
 /* bancoSangre.find({},function(err,docs){
  *     console.log(docs);
  *     console.log("Aqui")
@@ -75,11 +81,6 @@ app.post("/Imprimir", function(req,res){
 //app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/banSangre', express.static(path.join(__dirname, 'public')));
 //app.use('/hospital', express.static(path.join(__dirname, 'public')));
@@ -90,32 +91,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/hospital', hospit);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-    });
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 // error handler
 app.use(function(err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-    });
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 module.exports = app;
 
 var mongodbUri = 'mongodb://infobanco:usuario123@ds119081.mlab.com:19081/regalandovida';
 //var mongodbUri = 'mongodb://localhost:27017';
 mongoose.connect(mongodbUri, function(err){
-	if(err){
-	    console.error('Can\'t connect to MongoDB\n' + err)
-		return err;
-	}
-	console.log("MongoDB connected");
-    });
+    if(err){
+	console.error('Can\'t connect to MongoDB\n' + err)
+	return err;
+    }
+    console.log("MongoDB connected");
+});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -133,7 +134,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 //var hospital = mongoose.model('hospital', hospitalDB);
 //var usuario = mongoose.model('usuario', usuarioDB);
 //var silence = new banco({ name: 'Silence' });
-//console.log(silence.name);   
+//console.log(silence.name);
 
 /* var nose = new bancoSangre({
  *     idbanco: "bCruzRoja",
