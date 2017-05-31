e4a7bf38e4f008d42ef8b4647e27b7e84ecc66bd
Esteban Salazar montoya <Esteban@MacBook-Pro-de-Esteban-3.local>
2017-05-31 11:17:48 -0500

    carpetas y archisvos que no sirven ahora

--- regalandoVida/routes/hospital.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET hospital listing. */
-router.get('/', function(req, res, next) {
-	res.render('hospit', {title: 'Hospital'});
-});
-
-module.exports = router;
